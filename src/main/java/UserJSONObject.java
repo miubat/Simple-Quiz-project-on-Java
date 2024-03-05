6b20902a18b9b280e09c81736fb48d983410e3fa
Sanzida Khan <96487335+miubat@users.noreply.github.com>
2024-03-06 02:49:42 +0600

    java-json

---
+++ src/main/java/UserJSONObject.java
@@ -0,0 +1,37 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class UserJSONObject {
+    public static void main(String[] args) throws IOException, ParseException {
+       WriteJSONArray("admin", "1234", "admin");
+        WriteJSONArray("salman", "1234", "student");
+//        ReadJSONArray();
+
+    }
+
+    public static void WriteJSONArray(String username, String password, String role) throws IOException, ParseException {
+        String filename = "./src/main/resources/users.json";
+        JSONParser jsonParser = new JSONParser();
+        JSONArray usersArray = (JSONArray) jsonParser.parse(new FileReader(filename));
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", username);
+        jsonObject.put("password", password);
+        jsonObject.put("role", role);
+
+        usersArray.add(jsonObject);
+
+        FileWriter fw = new FileWriter(filename);
+        fw.write(usersArray.toJSONString());
+        fw.flush();
+        fw.close();
+    }
+
+}
